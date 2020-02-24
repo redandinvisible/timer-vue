be49000cee486d371d2f9efacf115202af5e6cd1
redandinvisible <rachel.wilson@theodi.com>
2020-02-24 13:11:23 +0000

    Add start, pause and restart buttons with listeners

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,14 +1,70 @@
 <template>
     <div>
-         {{ time_pattern }}
+        <div class="row">
+            <div>{{ minutes }}</div>
+            <div>:</div>
+            <div>{{ seconds }}</div>
+        </div>
+        <TimerButtons @buttonClicked="onTimerButtonClicked"/>
     </div>
 </template>
 
 <script>
+    import TimerButtons from "./TimerButtons";
+
     export default {
         name: "Timer",
+        components: {
+            TimerButtons
+        },
         props: {
             time_pattern: String
+        },
+        data() {
+            return {
+                minutes: "",
+                seconds: ""
+            }
+        },
+        watch: {
+            time_pattern: {
+                handler(pattern) {
+                    let vals = pattern.split(":").reverse();
+                    this.minutes = vals[1];
+                    this.seconds = vals[0];
+                }
+            }
+        },
+        methods: {
+            onTimerButtonClicked(id) {
+                switch(id) {
+                    case 'play':
+                        this.startCountdown();
+                        break;
+                    case 'pause':
+                        this.pauseCountdown();
+                        break;
+                    case 'startover':
+                        this.restartCountdown();
+                        break;
+                }
+            },
+            startCountdown() {
+                // eslint-disable-next-line no-console
+                return console.log("startCountdown");
+            },
+            pauseCountdown() {
+                // eslint-disable-next-line no-console
+                return console.log("pauseCountdown");
+            },
+            restartCountdown() {
+                // eslint-disable-next-line no-console
+                return console.log("restartCountdown");
+            },
+            timesUp() {
+                // eslint-disable-next-line no-console
+                return console.log("time's up!");
+            }
         }
     };
 </script>
---
+++ src/components/TimerButtons.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <div @click="onClick($event)" value="play"><img :src="'./images/play.svg'" id="play" width="50" height="50"></div>
+        <div @click="onClick($event)" value="pause"><img :src="'./images/pause.svg'" id="pause" width="50" height="50"></div>
+        <div @click="onClick($event)" value="startover"><img :src="'./images/start-over.svg'" id="startover" width="50" height="50"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TimerButtons",
+        methods: {
+            onClick(e) {
+                this.$emit('buttonClicked', e.target.id)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
